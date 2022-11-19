f1b43e9a0f8ae22f980996465be214f72e78bfc5
LeeTorres3 <lee.salomon.gomez.torres.2.b@gmail.com>
2022-11-18 20:15:31 -0600

    a

---
+++ .sql
@@ -0,0 +1,14 @@
+SELECT
+/*Sirve para hacer una consulta de datos*/
+
+FROM 
+/*Esta instrucción nos marca los datos de los usuarios, con (SELECT) le decimos que datos queremos saber*/
+
+SELECT name
+FROM users;
+/*Este ejemplo nos muestra los datos de los usuarios de la columna name (nombres)*/
+
+SELECT title
+FROM movies;
+/*Este ejemplo nos muestra la lista de títulos de películas de la tabla*/
+
